4a9d3a36aea2d4beab068ac2c432d28b89921789
Martin H <martin.homann@mailbox.org>
2024-01-15 18:18:08 +0100

    implemented pid

--- nodes/position_controller.py
+++ nodes/position_controller.py
@@ -11,6 +11,8 @@ from hippo_msgs.msg import ActuatorSetpoint
 from rclpy.node import Node
 from tf_transformations import euler_from_quaternion
 
+from final_project.msg import PIDStamped2
+
 def numpy_to_vector3(array: np.ndarray) -> Vector3:
     # function for safe and convenient type conversion
     arr = array.reshape((1,-1))
@@ -40,6 +42,12 @@ def vector3_rotate(vector3: np.ndarray, quaternion: np.ndarray) -> np.ndarray:
     q_inverse = np.concatenate(([quaternion[0]], -quaternion[1:])) # w, -x, -y, -z
     return quaternion_multiply(quaternion, quaternion_multiply(q_vec, q_inverse))[1:]
 
+def low_pass_filter(x: float, y_old: float, cutoff: float, 
+                    dt: rclpy.time.Time) -> float: # type: ignore
+    alpha = dt/(dt + 1/(2*np.pi*cutoff))
+    y = x * alpha + (1- alpha)*y_old
+    return y
+
 class PositionController(Node):
 
     def __init__(self):
@@ -55,8 +63,9 @@ class PositionController(Node):
         self.get_logger().info(f'{self.gains_x}')
 
         #
-        self.quaternion = np.zeros(4) # w, x, y, z
-
+        self.cutoff = 40.0 # cutoff frequency for low pass filter
+        self.last_filter_estimate= np.zeros(3) # low pass
+        self.last_error = np.zeros(3)
         self.error_integral = np.zeros(3)
         self.last_time = self.get_clock().now().nanoseconds * 10e-9
 
@@ -69,6 +78,11 @@ class PositionController(Node):
         
         self.setpoint = Point()
         self.setpoint_timed_out = True
+
+        # debug publisher
+        self.pid_debug_pub = self.create_publisher(msg_type=PIDStamped2,
+                                                topic='pid_gain',
+                                                qos_profile=1)
 
         # subscriber
         self.pose_sub = self.create_subscription(PoseWithCovarianceStamped,
@@ -84,7 +98,8 @@ class PositionController(Node):
                                 parameters=[
                                     ("gains_x", rclpy.Parameter.Type.DOUBLE_ARRAY),
                                     ("gains_y", rclpy.Parameter.Type.DOUBLE_ARRAY),
-                                    ("gains_z", rclpy.Parameter.Type.DOUBLE_ARRAY)
+                                    ("gains_z", rclpy.Parameter.Type.DOUBLE_ARRAY),
+                                    ("cutoff", rclpy.Parameter.Type.DOUBLE)
                                              ])
         
         param = self.get_parameter('gains_x')
@@ -96,6 +111,9 @@ class PositionController(Node):
         param = self.get_parameter('gains_z')
         self.gains_z = param.get_parameter_value().double_array_value
 
+        param = self.get_parameter('cutoff')
+        self.cutoff = param.get_parameter_value().double_value
+
     def on_params_changed(self, params):
         param: rclpy.Parameter
         for param in params:
@@ -106,6 +124,8 @@ class PositionController(Node):
                 self.gains_y = param.get_parameter_value().double_array_value
             elif param.name == 'gains_z':
                 self.gains_z = param.get_parameter_value().double_array_value
+            elif param.name == 'cutoff':
+                self.cutoff = param.get_parameter_value().double_value
             else:
                 continue
         return SetParametersResult(successful=True, reason='Parameter set')
@@ -127,26 +147,50 @@ class PositionController(Node):
             return
         position = msg.pose.pose.position
         q = msg.pose.pose.orientation
-        _, _, yaw = euler_from_quaternion([q.x, q.y, q.z, q.w])
+        #_, _, yaw = euler_from_quaternion([q.x, q.y, q.z, q.w])
         self.apply_control(position, q)
 
     def apply_control(self, position: Point, quaternion: Quaternion):
         now = self.get_clock().now()
-        x_error = self.setpoint.x - position.x
-        y_error = self.setpoint.y - position.y
-        z_error = self.setpoint.z - position.z
+        gain = np.array([self.gains_x, self.gains_y, self.gains_z])
 
-        x = 1.0 * x_error
-        y = 1.0 * y_error
-        z = 1.0 * z_error
-        thrust = np.array([x,y,z])
+        dt = self.get_clock().now().nanoseconds * 10e-9 - self.last_time
+
+        # sort out all abnormal dt
+        if dt > 1:
+            dt = 0.0
+
+        thrust = np.zeros(3)
+        error = np.array([self.setpoint.x-position.x, self.setpoint.y-position.y, self.setpoint.z-position.z])
+        error_change = np.zeros(3)
+        if dt != 0:
+            error_change = (error - self.last_error)/dt
+        derivative_error = np.zeros(3)
+        
+        for i in range(3):
+            if dt != 0:
+                derivative_error[i] = low_pass_filter(self.last_filter_estimate[i], error_change[i], cutoff=self.cutoff, dt=dt)
+
+            # integral
+            if np.abs(error[i]) < 0.05:
+                self.error_integral[i] = self.error_integral[i] + dt * error[i]
+            else:
+                self.error_integral[i] = 0
+        
+        # final PID calculation!
+        thrust = error * gain[:, 0] + self.error_integral * gain[:, 1] + derivative_error * gain[:, 2]
 
         # convert to local space of the robot
         q=np.array([quaternion.w,quaternion.x,quaternion.y,quaternion.z])
         thrust = vector3_rotate(vector3=thrust, quaternion=q)
         thrust = thrust.reshape(-1)
 
+        self.last_error = error
+        self.last_time = now.nanoseconds * 10e-9
         self.publish_thrust(thrust=thrust, timestamp=now)
+
+        # publish some information for debugging and documentation
+        self.publish_pid_info(gain, error, self.error_integral, derivative_error, timestamp=now)
 
     def publish_thrust(self, thrust: np.ndarray, 
                        timestamp: rclpy.time.Time) -> None: # type: ignore
@@ -157,6 +201,23 @@ class PositionController(Node):
         msg.z = np.float64(thrust[2])
 
         self.thrust_pub.publish(msg)
+
+    def publish_pid_info(self, gains: np.ndarray, error: np.ndarray, 
+                         i_error: np.ndarray, d_error:np.ndarray, 
+                         timestamp: rclpy.time.Time): # type: ignore
+        msg = PIDStamped2()
+
+        msg.gain_p = numpy_to_vector3(np.array([gains[0, 0],gains[1, 0],gains[2, 0]]))
+        msg.gain_i = numpy_to_vector3(np.array([gains[0, 1],gains[1, 1],gains[2, 1]]))
+        msg.gain_d = numpy_to_vector3(np.array([gains[0, 2],gains[1, 2],gains[2, 2]]))
+
+        msg.error = numpy_to_vector3(error)
+        msg.error_integral = numpy_to_vector3(i_error)
+        msg.error_derivative = numpy_to_vector3(d_error)
+
+        msg.header.stamp = timestamp.to_msg()
+        
+        self.pid_debug_pub.publish(msg)
 
 
 def main():
